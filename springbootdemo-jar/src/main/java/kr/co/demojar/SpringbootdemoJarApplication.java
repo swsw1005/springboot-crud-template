87091a3ea643c8068858fa120fa3275c095a7bee
swim <swsw1005@gmail.com>
2023-07-01 16:20:50 +0900

    first commit

---
+++ springbootdemo-jar/src/main/java/kr/co/demojar/SpringbootdemoJarApplication.java
@@ -0,0 +1,13 @@
+package kr.co.demojar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoJarApplication.class, args);
+    }
+
+}
---
+++ springbootdemo-jar/src/test/java/kr/co/demojar/SpringbootdemoJarApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.demojar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
